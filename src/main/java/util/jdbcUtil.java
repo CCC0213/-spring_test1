af60f5d87b231d1d8c90e483b662c7f02cf6a364
Your Name <you@example.com>
2022-05-23 17:28:52 +0800

    jdbcutil

--- src/main/java/util/jdbcUtil.java
+++ src/main/java/util/jdbcUtil.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class jdbcUtil {
@@ -11,10 +13,10 @@ public class jdbcUtil {
     private static Connection conn;
     private static InputStream in =null;
     private final static String CONF_NAME="jdbcDruid.properties";
-
+    private static ThreadLocal<Connection> threadLocal;
 
     static{
-
+        threadLocal=new ThreadLocal<Connection>();
         Properties pro =new Properties();
 
         try {
@@ -26,8 +28,63 @@ public class jdbcUtil {
             e.printStackTrace();
         }
 
-
     }
 
+
+    public static Connection getConn() {
+        Connection conn=threadLocal.get();
+        try {
+            if(conn==null) {
+                conn = dataSource.getConnection();
+                threadLocal.set(conn);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return conn;
+    }
+    /**
+     * 开启事务
+     * @throws SQLException
+     */
+    public static void beginTranscation() throws SQLException {
+        Connection connection=getConn();
+        connection.setAutoCommit(false);
+    }
+    /**
+     * 提交事务
+     * @throws SQLException
+     */
+    public static void commitTranscation() throws SQLException {
+        Connection connection=getConn();
+        connection.commit();
+    }
+    /**
+     * 回滚事务
+     * @throws SQLException
+     */
+    public static void rollbackTranscation() throws SQLException{
+        Connection connection=getConn();
+        connection.rollback();
+    }
+
+    public static void closeAll(Connection conn, PreparedStatement pstm){
+
+        if(pstm != null){
+            try {
+                pstm.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if(conn != null){
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    }
 
 }
